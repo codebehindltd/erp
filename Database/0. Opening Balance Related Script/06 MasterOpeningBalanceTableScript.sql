d3a88bb0986ec7c6bbf17e2605e9ae00904fa825
Mamun <codebehindltd@gmail.com>
2023-02-13 17:45:19 +0600

    Company Payment Related Issue Solved

--- Database/0. Opening Balance Related Script/06 MasterOpeningBalanceTableScript.sql
+++ Database/0. Opening Balance Related Script/06 MasterOpeningBalanceTableScript.sql
@@ -18,4 +18,13 @@ SELECT * FROM HotelCompanyOpeningBalance
 SELECT * FROM PMSupplierOpeningBalance
 SELECT * FROM PayrollEmployeeOpeningBalance
 SELECT * FROM LCCNFOpeningBalance
+SELECT * FROM LCCNFOpeningBalanceDetail
 SELECT * FROM MemberOpeningBalance
+
+----DROP TABLE GLOpeningBalance
+----DROP TABLE HotelCompanyOpeningBalance
+----DROP TABLE PMSupplierOpeningBalance
+----DROP TABLE PayrollEmployeeOpeningBalance
+----DROP TABLE LCCNFOpeningBalance
+----DROP TABLE LCCNFOpeningBalanceDetail
+----DROP TABLE MemberOpeningBalance
